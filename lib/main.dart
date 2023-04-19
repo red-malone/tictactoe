72e3228872ead91a04e9335081f43f5583f9ac71
Rohit Pradeep Sudarsanan <rohit.psudarsanan@gmail.com>
2023-04-19 09:29:34 +0530

    addition of functions

--- lib/main.dart
+++ lib/main.dart
@@ -31,8 +31,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int X=0;
-  int Y=0;
+  int _scoreX=0;
+  int _scoreY=0;
   bool _turnO=false;
   int filled=0;
   final List<String> _xolist=['','','','','','','','',''];
@@ -45,7 +45,11 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text("TICTACTOE"),
         backgroundColor: Colors.black,
         actions: [
-          IconButton(onPressed:(){}, icon: Icon(Icons.refresh))
+          IconButton(
+              onPressed:(){
+                _clearBoard();
+                },
+              icon: Icon(Icons.refresh))
         ],
       ),
       backgroundColor: Colors.grey,
@@ -73,7 +77,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   children: [
                     Text('Player X',style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.w800),),
                     SizedBox(height: 2),
-                    Text("Score",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)
+                    Text(_scoreX.toString(),style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)
                   ],
                 ),
 
@@ -83,7 +87,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   children: [
                     Text('Player O',style: TextStyle(color: Colors.white,fontSize: 30,fontWeight: FontWeight.w800),),
                     SizedBox(height: 2),
-                    Text("Score",style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)
+                    Text(_scoreY.toString(),style: TextStyle(fontSize: 20,fontWeight: FontWeight.w500),)
                   ],
                 ),
 
@@ -107,14 +111,16 @@ class _MyHomePageState extends State<MyHomePage> {
                 crossAxisCount:3 ),
             itemBuilder:(BuildContext context,int index){
               return GestureDetector(
-                onTap:(){},
+                onTap:(){
+                  _tapped(index);
+                },
                 child: Container(
                   decoration: BoxDecoration(border: Border.all(color: Colors.white)),
                   child: Center(
                     child: Text(
                       _xolist[index],style: TextStyle(
                       color: _xolist[index] =='x' ? Colors.amber : Colors.blue,
-                      fontSize: 10,
+                      fontSize: 36,
                     ),
                     ),
                   ),
@@ -137,4 +143,123 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
     );
   }
+  void _tapped(int index){
+    setState(() {
+      if(!_turnO && _xolist[index] == ''){
+        _xolist[index]='x';
+        filled+=1;
+      }
+      else if(_turnO && _xolist[index] == ''){
+        _xolist[index]='o';
+        filled+=1;
+      }
+
+      _turnO=!_turnO;
+      if(filled==3 || filled==5 || filled==7 ||  filled== 8 || filled ==9){
+        _wincheck();
+      }
+
+
+    });
+  }
+
+  void _wincheck(){
+    //row 1
+    if(_xolist[0]==_xolist[1] &&
+    _xolist[0]==_xolist[2] &&_xolist[0]!=''){
+      _showAlertDialog(context,"Winner", _xolist[0]);
+      return;
+    }
+    //row2
+    if (_xolist[3]==_xolist[4] &&
+        _xolist[3]==_xolist[5] &&_xolist[3]!=''){
+      _showAlertDialog(context,"Winner", _xolist[3]);
+      return;
+    }
+    //row3
+    if (_xolist[6]==_xolist[7] &&
+        _xolist[6]==_xolist[8] &&_xolist[7]!=''){
+      _showAlertDialog(context,"Winner", _xolist[7]);
+      return;
+    }
+    //col1
+    if (_xolist[0]==_xolist[3] &&
+        _xolist[0]==_xolist[6] &&_xolist[0]!=''){
+      _showAlertDialog(context,"Winner", _xolist[0]);
+      return;
+    }
+    //col2
+    if (_xolist[1]==_xolist[4] &&
+        _xolist[1]==_xolist[7] &&_xolist[1]!=''){
+      _showAlertDialog(context,"Winner", _xolist[1]);
+      return;
+    }
+    //col3
+    if(_xolist[2]==_xolist[5] &&
+        _xolist[2]==_xolist[8] &&_xolist[0]!=''){
+      _showAlertDialog(context,"Winner", _xolist[2]);
+      return;
+    }
+    if(_xolist[0]==_xolist[4] && _xolist[0]==_xolist[8] &&_xolist[0]==''){
+      _showAlertDialog(context,"Winner", _xolist[0]);
+      return;
+    }
+    if(_xolist[2]==_xolist[4] && _xolist[2]==_xolist[6] &&_xolist[2]==''){
+      _showAlertDialog(context,"Winner", _xolist[0]);
+      return;
+    }
+    if(filled==9){
+      _showAlertDialog(context, "Draw",'');
+    }
+
+  }
+
+  /*void _showAlertDialog(String title, String winner) {
+    showAlertDialog(
+        context: context,
+        title: title,
+        content: winner == '' ? 'The match ended in a draw' : 'The winner is ${winner.toUpperCase()}',
+        defaultActionText: 'OK',
+        onOkPressed: () {
+          if (winner == 'o') {
+             _scoreY+=1;
+          } else if (winner == 'x') {
+            _scoreX+= 1;
+          }
+          _clearBoard();
+          Navigator.of(context).pop();
+        }
+    );
+  }*/
+  void _showAlertDialog(BuildContext context, String title, String winner) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(winner == '' ? 'The match ended in a draw' : 'The winner is ${winner.toUpperCase()}'),
+          actions: <Widget>[
+            TextButton(
+              child: Text('OK'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+
+
+  void _clearBoard() {
+    setState(() {
+      for (int i=0;i<9;i++){
+        _xolist[i]='';
+      }
+    });
+    filled=0;
+  }
+
 }
